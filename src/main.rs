7bb493420aa0c3095639ab3cc0447458af425953
Spiryd <neumann.maks@gmail.com>
2023-09-01 13:34:53 +0200

    first try

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use tungstenite::{connect, Message};
+use url::Url;
+
 fn main() {
-    println!("Hello, world!");
+
+    let (mut socket, response) 
+        = connect(Url::parse("wss://ws.xtb.com/demo").unwrap()).expect("Can't connect");
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (ref header, _value) in response.headers() {
+        println!("* {}", header);
+    }
+
+    socket.send(Message::Text("Hello WebSocket".into())).unwrap();
+    loop {
+        let msg = socket.read().expect("Error reading message");
+        println!("Received: {}", msg);
+    }
 }
