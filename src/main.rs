e3ba0a96307c3b4ae8f48e77201ae9ad25c9e543
Spiryd <neumann.maks@gmail.com>
2023-09-04 15:10:56 +0200

    login working

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use tungstenite::{connect, Message};
 use url::Url;
+use std::env;
+use dotenv::dotenv;
+use indoc::formatdoc;
 
 fn main() {
-
+    //loading the environment variables
+    dotenv().ok();
+    //connecting to API socket
     let (mut socket, response) 
-        = connect(Url::parse("wss://ws.xtb.com/demo").unwrap()).expect("Can't connect");
+        = connect(Url::parse("wss://ws.xtb.com/real").unwrap()).expect("Can't connect");
     println!("Connected to the server");
     println!("Response HTTP code: {}", response.status());
-    println!("Response contains the following headers:");
-    for (ref header, _value) in response.headers() {
-        println!("* {}", header);
-    }
-
-    socket.send(Message::Text("Hello WebSocket".into())).unwrap();
+    //logging in
+    let userid = env::var("USER_ID").expect("You've not set the USER_ID");
+    let password = env::var("PASSWORD").expect("You've not set the PASSWORD");
+    let login = formatdoc!{r#"
+        {{
+            "command" : "login",
+            "arguments" : {{
+                "userId" : "{userid}",
+                "password": "{password}"
+            }}
+        }}"#, userid=userid, password=password,};
+    socket.send(Message::Text(login.into())).unwrap();
     loop {
         let msg = socket.read().expect("Error reading message");
         println!("Received: {}", msg);
